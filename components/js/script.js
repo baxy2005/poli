d677f4ca8cd176cba0cf170d197995aa389485a7
Varadi Andris <andris@varadi.tv>
2016-01-20 17:08:38 +0100

    navigation, background color shifting, calendar and so on

--- components/js/script.js
+++ components/js/script.js
@@ -12,5 +12,36 @@ $('#news-carousel').owlCarousel({
     loop:true,
 	autoplay: false,
 });
+$('#calendar-carousel').owlCarousel({
+    items:1,
+    lazyLoad:true,
+    loop:true,
+	autoplay: false,
+});
 
   
+$(function() {
+  
+  // Init ScrollMagic Controller
+  var scrollMagicController = new ScrollMagic();
+  
+
+
+    var scene = new ScrollScene({
+    offset: 60 /* offset the trigger 150px below #scene's top */
+	
+  })
+    .setClassToggle('.navbar-toggle', 'navbar-toggle-fixed')
+    .addTo(scrollMagicController);
+	
+	  var scene2 = new ScrollScene({
+		offset: 800
+  })
+  .setClassToggle('body', 'scene-2-active')
+  .addTo(scrollMagicController);
+  
+  // Add debug indicators fixed on right side
+   scene.addIndicators();
+   
+ 
+});
