b2fa9c2420676a43c324ef342faa03511220a8fa
Varadi Andris <andris@varadi.tv>
2016-02-17 16:38:59 +0100

    secondary sticky navigation

--- components/js/script.js
+++ components/js/script.js
@@ -34,12 +34,27 @@ $(function() {
     .setClassToggle('.navmenu', 'navmenu-fixed')
     .addTo(scrollMagicController);
 	
-	  var scene2 = new ScrollScene({
+	/*  var scene2 = new ScrollScene({
 		offset: 600
   })
   .setClassToggle('body', 'scene-2-active')
-  .addTo(scrollMagicController);
+  .addTo(scrollMagicController);*/
   
+  var scene3 = new ScrollScene({
+    offset: 605 /* offset the trigger 150px below #scene's top */
+	
+  })
+    .setClassToggle('.secondary-nav', 'secondary-nav-top')
+    .addTo(scrollMagicController);
+	
+	
+	 var scene4 = new ScrollScene({
+    offset: 60 /* offset the trigger 150px below #scene's top */
+	
+  })
+    .setClassToggle('.form-inline-opening', 'form-inline-opening-fixed')
+    .addTo(scrollMagicController);
+	
   // Add debug indicators fixed on right side
    scene.addIndicators();
    
