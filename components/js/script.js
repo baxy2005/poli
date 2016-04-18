39be4c416889f7a78bdb10b276b2c85677ca9fef
Varadi Andris <andris@varadi.tv>
2016-04-18 09:22:08 +0200

    secondary navigation update

--- components/js/script.js
+++ components/js/script.js
@@ -1,5 +1,3 @@
-// JavaScript Document
-
 $('.custom1').owlCarousel({
     items:1,
     lazyLoad:false,
@@ -57,9 +55,36 @@ $('#calendar-carousel').owlCarousel({
 $(function() {
   
   // Init ScrollMagic Controller
-  var scrollMagicController = new ScrollMagic();
-  
+  var scrollMagicController = new ScrollMagic({
+    globalSceneOptions: {
+      triggerHook: "onLeave"
+    }
+  });
 
+  //pin the navigation
+  var pin = new ScrollScene({
+    triggerElement: '#nav-trigger',
+	offset: 5  /*offset the trigger 150px below #scene's top */
+  })
+  .setPin('.secondary-nav')
+  .addTo(scrollMagicController);
+ 
+    var scene3 = new ScrollScene({
+	triggerElement: '#nav-trigger',
+  	offset: 6  /*offset the trigger 150px below #scene's top */
+
+  })
+    .setClassToggle('.secondary-nav', 'secondary-nav-top')
+   .addTo(scrollMagicController);
+
+	 var scene6 = new ScrollScene({
+	triggerElement: '#nav-trigger',
+  	offset: 120  /*offset the trigger 150px below #scene's top */
+	
+  })
+    .setClassToggle('.secondary-nav', 'fade-secondary-nav-bg')
+    .addTo(scrollMagicController);
+	
 
     var scene = new ScrollScene({
     offset: 60 /* offset the trigger 150px below #scene's top */
@@ -70,44 +95,17 @@ $(function() {
 	
 	
 	
-	/*  var scene2 = new ScrollScene({
-		offset: 600
-  })
-  .setClassToggle('body', 'scene-2-active')
-  .addTo(scrollMagicController);*/
-  
-  var scene3 = new ScrollScene({
-    offset: 585 /* offset the trigger 150px below #scene's top */
 	
-  })
-    .setClassToggle('.secondary-nav', 'secondary-nav-top')
-    .addTo(scrollMagicController);
-	
-	 var scene5 = new ScrollScene({
-    offset: 585 /* offset the trigger 150px below #scene's top */
-	
-  })
-    .setClassToggle('#main-carousel', 'carusel-margin-add')
-    .addTo(scrollMagicController);
-	
-		 var scene6 = new ScrollScene({
-    offset: 665 /* offset the trigger 150px below #scene's top */
-	
-  })
-    .setClassToggle('.secondary-nav', 'fade-secondary-nav-bg')
-    .addTo(scrollMagicController);
-	
-	
-	var tween = TweenMax.from('.brand-on', 0.2, {
-    opacity: 0,
-  });
-		 var scene7 = new ScrollScene({
-			  triggerElement: '#brand-switch',
-			   offset: -15
-	
-  })
-    .setTween(tween)
-    .addTo(scrollMagicController);
+	//var tween = TweenMax.from('.brand-on', 0.2, {
+//    opacity: 0,
+//  });
+//		 var scene7 = new ScrollScene({
+//			  triggerElement: '#brand-switch',
+//			   offset: -15
+//	
+//  })
+//    .setTween(tween)
+//    .addTo(scrollMagicController);
 	
 	
 	 var scene4 = new ScrollScene({
